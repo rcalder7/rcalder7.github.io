d834df4057d41a9e93ef4fd565682b27e421daa6
rcalder7 <46700832+rcalder7@users.noreply.github.com>
2019-03-17 16:13:43 -0500

    Update app.js

--- todow3/src/js/app.js
+++ todow3/src/js/app.js
@@ -1,7 +1,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/todow3/sw.js')
+    .register('rcalder7.github.io/todow3/sw.js')
     .then(function() {
       console.log('Service worker registered!');
     });
